e65233807464985b8d3b54e59528df2dd78299d3
Yuliya Simonchyk <y.simonchyk@softteco.com>
2024-01-15 14:53:02 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,11 @@ dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3000,
+  define: {
+    'process.env': process.env,
   },
+  server: {
+    host: true,
+  },
+  base: './',
 });
